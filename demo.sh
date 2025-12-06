159c5e8c6c2bf9604d72eddb3b40ee627a06ed3c
jasonz <ksdpmx@gmail.com>
2025-12-06 11:53:43 +0000

    init

---
+++ demo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source ./env.sh
+
+sudo "$kind" create cluster --config ./cluster.yaml --name cluster-1
+sudo "$kind" create cluster --config ./cluster.yaml --name cluster-2
+
+sudo "$kind" get kubeconfig --name cluster-1 > kubeconfig-1.yaml
+sudo "$kind" get kubeconfig --name cluster-2 > kubeconfig-2.yaml
+
+KUBECONFIG=kubeconfig-1.yaml:kubeconfig-2.yaml "$kubectl" config view --flatten > kubeconfig
+[[ -f ./kubeconfig-1.yaml ]] && rm kubeconfig-1.yaml
+[[ -f ./kubeconfig-2.yaml ]] && rm kubeconfig-2.yaml
+
+>&2 echo "please use ./k1 and ./k2 for kubectl"
---
+++ env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+kind="./kind"
+kubectl="./kubectl"
